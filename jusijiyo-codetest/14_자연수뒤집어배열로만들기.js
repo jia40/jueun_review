0630f38429578fdb4cc81601b5b45e473c980efd
jia40 <lsc7439@g.eulji.ac.kr>
2026-04-05 23:43:25 +0900

    jusijiyocodetest

---
+++ jusijiyo-codetest/14_자연수뒤집어배열로만들기.js
@@ -0,0 +1,11 @@
+// 자연수 n을 뒤집어 각 자리 숫자를 원소로 가지는 배열 형태로 리턴해주세요. 
+// 예를들어 n이 12345이면 [5,4,3,2,1]을 리턴합니다.
+function solution(n) {
+    var answer = [];
+    var arr=n.toString().split('');
+    
+    for(var i=arr.length-1;i>=0; i--){
+        answer.push(Number(arr[i]));
+    }
+    return answer;
+}
